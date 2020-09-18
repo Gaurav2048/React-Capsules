7e9621d85b7934be7880e7cda129383cd52a1afb
Gunjan Kalita <gunjankalita836@gmail.com>
2020-09-18 20:47:53 +0530

    Add drag and drop feature to the bullets

--- src/bullet-dnd/Item.js
+++ src/bullet-dnd/Item.js
@@ -1,13 +1,46 @@
-import React from "react";
-import cancel from "./assets/cancel.png";
+import React from 'react';
+import cancel from './assets/cancel.png';
 
-const Item = ({ item, style, onClick }) => {
-    return <div
-        onClick={e => onClick(e)}
-        style={{ ...style, display: 'flex', flexDirection: 'row', margin: '5px', alignItems: 'center' }} draggable >
-        <div style={{ display: 'inline' }} > {item} </div>
-        <img src={cancel} alt="cancel widget" style={{ width: '10px', height: '10px', backgroundColor: '#DDDDDD', padding: '5dp', cursor: 'pointer', marginLeft: '10px' }} />
+const Item = ({
+  item,
+  style,
+  onClick,
+  onDragEnter,
+  onDragStart,
+  onDragEnd,
+  position,
+}) => {
+  return (
+    <div
+      onClick={(e) => onClick(e)}
+      style={{
+        ...style,
+        display: 'flex',
+        flexDirection: 'row',
+        margin: '5px',
+        alignItems: 'center',
+      }}
+      onDragStart={(e) => onDragStart(e, position)}
+      onDragEnter={(e) => onDragEnter(e, position)}
+      onDragEnd={(e) => onDragEnd(e, position)}
+      onDragOver={(e) => e.preventDefault()}
+      draggable
+    >
+      <div style={{ display: 'inline' }}> {item} </div>
+      <img
+        src={cancel}
+        alt="cancel widget"
+        style={{
+          width: '10px',
+          height: '10px',
+          backgroundColor: '#DDDDDD',
+          padding: '5dp',
+          cursor: 'pointer',
+          marginLeft: '10px',
+        }}
+      />
     </div>
-}
+  );
+};
 
-export default Item; 
+export default Item;
--- src/bullet-dnd/index.js
+++ src/bullet-dnd/index.js
@@ -1,26 +1,60 @@
-import React from 'react'
-import Item from "./Item";
-import TextInput from "./TextInput";
+import React, { useRef } from 'react';
+import Item from './Item';
+import TextInput from './TextInput';
 
-const ReactBulletDnd = ({ children, style }) => {
-    const onClick = (e) => {
-        console.log('this is a click event of ', e.target);
+const ReactBulletDnd = ({ children, style, data, set }) => {
+  const draggingItem = useRef(null);
+  const draggingItemIndex = useRef(null);
+  const draggEnterIndex = useRef(null);
 
+  const onClick = (e) => {
+    console.log('this is a click event of ', e.target);
+  };
+
+  const onDragStart = (e, index) => {
+    draggingItem.current = data[index];
+    draggingItemIndex.current = index;
+  };
+
+  const onDragEnter = (e, index) => {
+    if (draggingItemIndex.current === index) return;
+    if (index === null || draggEnterIndex.current === index) return;
+    draggEnterIndex.current = index;
+    const ItemCopy = JSON.parse(JSON.stringify(data));
+    ItemCopy.splice(draggingItemIndex.current, 1);
+    console.log(ItemCopy);
+    if (draggingItemIndex.current < index) {
+      ItemCopy.splice(index - 1, 0, draggingItem.current);
+    } else {
+      ItemCopy.splice(index, 0, draggingItem.current);
     }
-    return <div style={{ ...style }} >
-        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}  >
+    console.log(ItemCopy);
+    set(ItemCopy);
+    console.log(index);
+  };
 
-            {React.Children.map(children, child => {
-                return React.cloneElement(child, {
-                    onClick: onClick
-                })
-            })}
+  const onDragEnd = (e, index) => {
+    draggingItem.current = null;
+    draggingItemIndex.current = null;
+  };
 
-        </div>
+  return (
+    <div style={{ ...style }}>
+      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+        {React.Children.map(children, (child) => {
+          return React.cloneElement(child, {
+            onClick,
+            onDragEnter,
+            onDragStart,
+            onDragEnd,
+          });
+        })}
+      </div>
     </div>
-}
+  );
+};
 
 ReactBulletDnd.Item = Item;
 ReactBulletDnd.TextInput = TextInput;
 
-export default ReactBulletDnd; 
+export default ReactBulletDnd;
